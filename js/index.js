bb898f251bdb87069cf384fe8a5ccb34c3de1818
RPB <sumitomo.andi@gmail.com>
2023-06-24 00:21:38 +0700

    edit radio

--- js/index.js
+++ js/index.js
@@ -3,3 +3,17 @@ function dont() {
         alert("No, You are Ugly!!");}
         document.getElementById("ugly").checked = false;
     }
+
+
+
+
+function img(x){
+    const y = document.getElementById("bite")
+    const z = document.getElementById("yay")
+    if(x==0)
+        y.style.display = "block";
+    else
+        y.style.display = "none";
+    return;
+}
+
